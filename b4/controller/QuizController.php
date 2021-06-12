5e544462ddf10b291c1805afcfd06c337a74b94e
nguyenbnam <nguyen.bnam.34@gmail.com>
2021-06-12 12:25:23 +0700

    đã gom

--- b4/controller/QuizController.php
+++ b4/controller/QuizController.php
@@ -3,87 +3,46 @@ class QuizController extends BaseController {
     protected $model; 
     public function __construct() {
         $this->model = $this -> model('model');
-
     }
     public function _getQuiz()
     {
         $questions =  $this->model->getData('question');
-        // echo $questions;die; 
         $options =  $this->model->getData('options');
-        // gộp chung đáp án đúng với cái nay
         $result =  $this->model->getData('result');
-        /**
-        * để cho ra 1 câu hỏi cà các đáp án thì sẽ là: 
-        * id [chứa câu hỏi][
-        *      chứa các lựa chọn - 
-        *      - chứa câu trả lời 
-        * ]
-        * [id question][question] = cau hoi; 
-        * trong câu hỏi chứa câu trả lời; 
-        * [id question][lựa chọn] = câu trả lời; 
-        * bên view se
-        */
         $array_questions = array();
         $array_options = array(); 
-        //$array_result = array(); 
         foreach($questions as $key =>$value) {
             $tmp = explode('|', $value);
-            // echo '<pre>'; 
-            // print_r($tmp);die; 
-            $id = $tmp[0]; // lấy id câu hỏi số
-            $question =$tmp[1];  // gán câu hỏi với id 
-            $array_questions[$id] = array('question'=>$question); // gộp cái câu hỏi với id câu hỏi số; 
+            $id = $tmp[0]; 
+            $question =$tmp[1];
+            $array_questions[$id] = array('question'=>$question);
         }
         foreach($options as $key => $value){
             $tmp = explode('|', $value); 
-            // echo '<pre>'; 
-            // print_r($tmp);die; 
-            $id = $tmp[0]; // lấy id câu hỏi
-            $option = $tmp[1]; // lấy các lựa chọn
-            $answer = $tmp[2]; // gán các đáp án
-            // và gộp các đáp án với câu hỏi + lựa chọn 
-            $array_options[$id][$option]= [$answer]; // gán giá trị vào mảng 2 chiều 
+            $id = $tmp[0]; 
+            $option = $tmp[1]; 
+            $answer = $tmp[2];
+            $array_options[$id][$option]= [$answer];
         }
         foreach($result as $key => $value){
             $tmp = explode('|', $value); 
-            // echo '<pre>'; 
-            // print_r($tmp);die; 
-            $id = $tmp[0]; // lấy id câu hỏi
-            $option = $tmp[1]; // gán các kq
-            $result = $tmp[2]; // gán các kq
-            // và gộp các đáp án với câu hỏi + lựa chọn 
-            $array_result[$id][$option]= [$result]; // gán giá trị vào mảng 2 chiều 
+            $id = $tmp[0]; 
+            $option = $tmp[1];
+            $result = $tmp[2]; 
+            $array_result[$id][$option]= [$result]; 
         }
-        // echo '<pre>';
-        // print_r($array_result); die;
-        // echo '<pre>';
-        // print_r($array_options); die;
-        // echo '<pre>';
-        // print_r($array_questions); die;
-        // gộp câu hỏi và các lựa chọn vào mảng 2 chiều; 
-        $arr_question_all = array(); // tạo ra 1 mảng rỗng để chứa câu hỏi và các lựa chọn 
-        foreach($array_questions as $key => $value){    //lặp cái câu hỏi để lấy id câu hỏi gộp cho các lựa chọn 
-            $arr_question_all[$key]['question'] = $value['question']; //gán câu hỏi với id cho từng câu
-            $arr_question_all[$key]['option'] = $array_options[$key]; // gán các option cho câu hỏi đó
-            // gán thêm cho n cái đáp án 
-            //$arr_question_all[$key]['result']= $array_result[$key]; // $array_result[$key] là giá trị truong cai mang đó 
+        $arr_question_all = array(); 
+        foreach($array_questions as $key => $value){
+            $arr_question_all[$key]['question'] = $value['question']; 
+            $arr_question_all[$key]['option'] = $array_options[$key]; 
         }
-        // echo '<pre>';
-        // print_r($arr_question_all); die;
-        //đẩy n ra view; 
         $this->view('quiz', [
         'arr_question_all'=>$arr_question_all
         ]);
     }
-
     public function _result()
     {
-        // gộp chung đáp án đúng với cái nay
         $result =  $this->model->getData('result');
-
-        // echo 'oi doi oi'; 
-        // echo '<pre>'; 
-        // print_r($result); die; 
         $array_result = array(); 
         foreach($result as $key => $value){
             $tmp = explode('|', $value); 
@@ -91,24 +50,13 @@ class QuizController extends BaseController {
             $option = $tmp[1]; 
             $answer = $tmp[2]; 
             $array_result[$id][$option] = $answer;
-            // handle
         }
-        // echo '<pre>'; 
-        // print_r($array_result); die;
         array_pop($_POST);  
-        // print_r($_POST); die; 
         $res = array();
         $_true = 0;  
         foreach($array_result as $key => $value){
-        // print_r($value);
-        // var_dump((int)trim($key));die;
         $key=(int)trim($key); 
-        // var_dump(trims($_POST[$key]));die; 
-        
         foreach($value as $key_child => $value_child){
-            // var_dump($key_child); die;
-            // $res = ($key_child==$_POST[$key])? 'dung' : 'sai';
-            // $resq = ($_POST[$key]==$key_child)? 'dung' : 'sai';
             if(trim($_POST[$key])==trim($key_child)){
                 $resq = true;
                 $_true+=1;
@@ -116,17 +64,10 @@ class QuizController extends BaseController {
                 else {
                     $resq = false;
             }
-            // lấy nốt đáp án. xong gán sang bên kia. ok 
-            // nếu dung thì cho n điểm k đúng thì khoanh đỏ nó; là xong. sáng mai chốt r sang SQL; 
         }
-        // echo $res; 
-        
         $res[$key]['result']= $resq; 
         $res[$key]['_true']= $_true; 
-
     }   
-    // echo '<pre>'; 
-        // print_r($res); 
         $this->view('viewResult', [
         'res'=> $res, 
         '_true'=>$_true,
--- b4/views/quiz.php
+++ b4/views/quiz.php
@@ -1,12 +1,9 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
+    <title>Quiz</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
@@ -24,24 +21,19 @@
           foreach($arr_question_all as $key=>$values) : ?>
           <label  for="">Câu hỏi <?php echo $key; echo ': '.$values['question']?></label>
           <?php foreach($values['option'] as $key_child=>$value_child) : ?>
-          <?php  //print_r($key)//print_r($key_child); print_r($value_child); ?>
           <div class="form-check">  
             <input type="radio" name="<?php echo $i ?>"  value="<?php echo $key_child ?>" required />
             <label  for=""><?php echo $key_child.': '.$value_child[0] ?></label>
           </div>
-          <?php endforeach;  $i++; // vong for cua thang con values?>
+          <?php endforeach;  $i++; ?>
           
-          <?php endforeach; // vong for cua thang arr_question?>
+          <?php endforeach;?>
 
           <button type="submit" class="btn btn-dark" name="submit">Hoàn thành</button>
         </form>
       </div>
     </div>
-
   </div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
